0a462f7415b2c17ff6213c7749aab621a93ed116
Craig Amiano <craigamiano@gmail.com>
2014-07-11 16:16:21 -0400

    Created Squaring Test

---
+++ square.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int x =0;
+	while(x < 10){
+	double y = sqrt((double)x);
+	cout << "The square root of " << x << " is " << y << endl;
+	x++;
+	}
+	return 0;
+}

6a6ccfb7109ee00c671ffd07674f322b157306d9
Craig Amiano <craigamiano@gmail.com>
2014-07-18 13:56:15 -0400

    added arrays and function tutorials/walkthroughs

---
+++ c++array.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;//delcare a variable 'k' that holds integer values
+    int ages [5];
+    ages={15,24,54,33,120};
+
+	for (k=0; k<5; k++)//set 'k' to 0 execute while 'k' is less than 5
+	   {
+		cout<<ages[k]<<endl;
+	   }
+    
+return 0;
+}

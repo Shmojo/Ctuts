99231067fc8e0b1994a5fc971a1f332a6e54ab15
Craig Amiano <craigamiano@gmail.com>
2014-07-22 14:06:12 -0400

    revised with multiple inheritances

--- c++inheritance.c
+++ c++inheritance.c
@@ -3,8 +3,7 @@
 using namespace std;
 
 // Base class
-class Shape 
-{
+class Shape {
    public:
       void setWidth(int w)
       {
@@ -19,9 +18,15 @@ class Shape
       int height;
 };
 
-// Derived class
-class Rectangle: public Shape
-{
+// Base Class PaintCost
+class PaintCost {
+   public:
+      int getCost(int area){
+         return area * 70;
+      }
+};
+//Derived class
+class Rectangle: public Shape, public PaintCost {
    public:
       int getArea()
       { 
@@ -29,15 +34,18 @@ class Rectangle: public Shape
       }
 };
 
-int main(void)
-{
+int main(void) {
    Rectangle Rect;
+   int area;
  
    Rect.setWidth(5);
    Rect.setHeight(7);
 
-   // Print the area of the object.
+   area = Rect.getArea();
+// Print the area of the object.
    cout << "Total area: " << Rect.getArea() << endl;
+// Print the total cost of painting
+   cout << "Total paint cost: $" << Rect.getCost(area) << endl;
 
    return 0;
 }
